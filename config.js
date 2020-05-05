218d00668c2e11187bfa589da57771e0a0a9311c
Julian Chamleunsouk <jchamleunsouk@gmail.com>
2020-05-05 19:47:53 -0400

    initial
    version 1

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteTitle: 'somsanouk.com', // <title>
+  manifestName: 'somsanoukjulianchamleunsouk',
+  manifestShortName: 'somsanouk', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'somsnaouk.com',
+  heading: 'Under Construction - May 2020',
+  // social
+  socialLinks: [
+
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:somsanoukagency@gmail.com',
+    },
+  ],
+};
