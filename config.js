7d52dfe27d3683ce075efd66ee5a2f222ebea10b
Julian Chamleunsouk <jchamleunsouk@gmail.com>
2021-03-04 14:39:01 -0500

    Update config.js
    updated header

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'somsanouk.com',
-  heading: 'Under Construction... Have a good summer! - Jules',
+  heading: 'New Everything Coming Summer 2021!',
   // social
   socialLinks: [
 
